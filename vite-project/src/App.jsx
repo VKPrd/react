18bc7c214108b0e5c7e64601da59e2c13d03d60f
vivek <vk0220CS191@gmail.com>
2024-09-29 12:16:47 +0530

    container config done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+      <h1>chai aur code vite</h1>
+      <Chai />
+    </>
+  );
+}
+export default App;
---
+++ vite-project/src/chai.jsx
@@ -0,0 +1,4 @@
+function Chai() {
+  return <h1>chai vite per ready ho gayi hii</h1>;
+}
+export default Chai;
